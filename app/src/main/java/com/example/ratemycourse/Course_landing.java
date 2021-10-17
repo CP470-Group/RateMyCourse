df9aa3c77ea55f13f62b78b6a32f4123f18a1b53
Muntake <lali3610@mylaurier.ca>
2021-10-17 12:45:17 -0700

    -Merged my code to the Master Branch

--- app/src/main/java/com/example/ratemycourse/Course_landing.java
+++ app/src/main/java/com/example/ratemycourse/Course_landing.java
@@ -19,6 +19,7 @@ public class Course_landing extends AppCompatActivity {
 
 
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
--- app/src/main/java/com/example/ratemycourse/new_review.java
+++ app/src/main/java/com/example/ratemycourse/new_review.java
@@ -8,6 +8,7 @@ import android.widget.Button;
 public class new_review extends AppCompatActivity {
     //Will create a function for this later for now this does not have to do anything.
     private Button submit;
+    private Button hi;
 
 
     @Override
