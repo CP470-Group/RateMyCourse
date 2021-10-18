d04a872e1fc4b7887e695c5b91e869d76d1525f5
EirkinDahi <eirkin12@gmail.com>
2021-10-18 17:16:38 -0400

    Fixed errors and added connectivity to homepage from sign-in/up pages

--- app/src/main/java/com/example/ratemycourse/LoginActivity.java
+++ app/src/main/java/com/example/ratemycourse/LoginActivity.java
@@ -2,13 +2,30 @@ package com.example.ratemycourse;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+
+    Button loginPageButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        loginPageButton = findViewById(R.id.loginPageButton);
+        loginPageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, HomePage.class);
+                startActivity(intent);
+            }
+        });
     }
+
+
 }
--- app/src/main/java/com/example/ratemycourse/RegisterActivity.java
+++ app/src/main/java/com/example/ratemycourse/RegisterActivity.java
@@ -2,13 +2,25 @@ package com.example.ratemycourse;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RegisterActivity extends AppCompatActivity {
-
+    Button signUpPageButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        signUpPageButton = findViewById(R.id.signUpPageButton);
+        signUpPageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(RegisterActivity.this, HomePage.class);
+                startActivity(intent);
+            }
+        });
     }
 }
