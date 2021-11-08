21473cf5518d4946e7b3c5b7c462dc267188ade4
mbarton281 <37551607+mbarton281@users.noreply.github.com>
2021-11-08 01:06:46 -0500

    -Adding the ability to search for schools -Adding the search course files for next step

--- app/src/main/java/com/example/ratemycourse/SchoolSearch.java
+++ app/src/main/java/com/example/ratemycourse/SchoolSearch.java
@@ -1,61 +1,208 @@
 package com.example.ratemycourse;
 
 //Imports
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.SearchView;
+import android.widget.Spinner;
 import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import java.util.ArrayList;
+import java.util.Locale;
 
 //This activity is where the user is going to search for the school they want to find a course for
 public class SchoolSearch extends AppCompatActivity {
 
+    //Database Constants
+    public static final String SCHOOL_ID = "schoolid";
+    public static final String SCHOOL_NAME = "schoolname";
+
     //Class variables
     SearchView searchView;
-    ListView listView;
-    ArrayList<String> list;
-    ArrayAdapter<String > adapter;
+    ListView listViewSchools;
+    ArrayList<School> schoolList;
+    DatabaseReference databaseSchools;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_school_search);
 
+        //getting school database
+        databaseSchools = FirebaseDatabase.getInstance().getReference("schools");
+
         //Find the views by there ids
         searchView = (SearchView) findViewById(R.id.searchSchools);
-        listView = (ListView) findViewById(R.id.listOfSchools);
+        listViewSchools = (ListView) findViewById(R.id.listOfSchools);
 
-        //This is temporary data while setting up the visual aspects of the activity.
-        //The final product will be searching the db for the required data
-        list = new ArrayList<>();
-        list.add("WLU");
-        list.add("Queens");
-        list.add("MUN");
+        schoolList = new ArrayList<>();
 
-        //Using simple_list_item_1 as it is all we need for the basic layout
-        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,list);
-        listView.setAdapter(adapter);
-
-        //Adding the listener to the searchView
-        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-            //Setting up some basic logic for searching for each school.
-            //This is not the finished product of course as we don't have the db setup and navigation to each school setup
+        listViewSchools.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public boolean onQueryTextSubmit(String query) {
-                if(list.contains(query)){
-                    adapter.getFilter().filter(query);
-                }else{
-                    Toast.makeText(SchoolSearch.this, "No Match found",Toast.LENGTH_LONG).show();
-                }
-                return false;
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                School school = schoolList.get(i);
+                Intent intent = new Intent(getApplicationContext(), AddCourseActivity.class);
+                intent.putExtra(SCHOOL_ID, school.getSchoolID());
+                intent.putExtra(SCHOOL_NAME, school.getSchoolName());
+                startActivity(intent);
             }
+        });
 
+        listViewSchools.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
-            public boolean onQueryTextChange(String newText) {
-                return false;
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+                School school = schoolList.get(i);
+                showUpdateDialog(school.getSchoolID(), school.getSchoolName() );
+                return true;
+            }
+        });
+
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        if(databaseSchools != null){
+            databaseSchools.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    if(dataSnapshot.exists()){
+                        schoolList = new ArrayList<>();
+                        for(DataSnapshot ds : dataSnapshot.getChildren()){
+                            schoolList.add(ds.getValue(School.class));
+                        }
+                        SchoolList adapter = new SchoolList(SchoolSearch.this, schoolList);
+                        listViewSchools.setAdapter(adapter);
+                    }
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+                    Toast.makeText(SchoolSearch.this, databaseError.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+        if (searchView != null){
+            searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+                @Override
+                public boolean onQueryTextSubmit(String schoolName) {
+                    return false;
+                }
+
+                @Override
+                public boolean onQueryTextChange(String schoolName) {
+                    searchSchool(schoolName);
+                    return true;
+                }
+            });
+        }
+
+    }
+
+    private void searchSchool(String schoolName){
+        ArrayList<School> schoolTempList = new ArrayList<>();
+        for(School object:schoolList){
+            if(object.getSchoolName().toLowerCase().contains(schoolName.toLowerCase())){
+                schoolTempList.add(object);
+            }
+        }
+        SchoolList adapter = new SchoolList(SchoolSearch.this, schoolTempList);
+        listViewSchools.setAdapter(adapter);
+    }
+
+    private void showUpdateDialog(String schoolID, String schoolName) {
+
+        // create alert dialog builder
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
+
+        // inflate xml layout
+        LayoutInflater inflater = getLayoutInflater();
+
+        // create view for dialog and inflate
+        final View dialogView = inflater.inflate(R.layout.update_dialog, null);
+
+        // set view for dialog builder as the inflated view
+        dialogBuilder.setView(dialogView);
+
+        final EditText updateSchoolName = (EditText) dialogView.findViewById(R.id.updateSchoolName);
+        final Spinner updateSpinner = (Spinner) dialogView.findViewById(R.id.updateSpinnerSchool);
+        final EditText updateSchoolCity = (EditText) dialogView.findViewById(R.id.updateSchoolCity);
+        final EditText updateSchoolEmail = (EditText) dialogView.findViewById(R.id.updateSchoolEmail);
+        final EditText updateSchoolAddress = (EditText) dialogView.findViewById(R.id.updateSchoolAddress);
+        final EditText updateSchoolPhone = (EditText) dialogView.findViewById(R.id.updateSchoolPhone);
+        final EditText updateSchoolPostalCode = (EditText) dialogView.findViewById(R.id.updateSchoolPostalCode);
+
+        final Button updateButton = (Button) dialogView.findViewById(R.id.buttonUpdateSchool);
+        final Button deleteButton = (Button) dialogView.findViewById(R.id.buttonDelete);
+
+        dialogBuilder.setTitle("Updating School: " + schoolName);
+
+        AlertDialog alertDialog = dialogBuilder.create();
+        alertDialog.show();
+
+        updateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = updateSchoolName.getText().toString().trim();
+                String province = updateSpinner.getSelectedItem().toString();
+                String city = updateSchoolCity.getText().toString().trim();
+                String email = updateSchoolEmail.getText().toString().trim();
+                String address = updateSchoolAddress.getText().toString().trim();
+                String phone = updateSchoolPhone.getText().toString().trim();
+                String postalCode = updateSchoolPostalCode.getText().toString().trim();
+                if (!TextUtils.isEmpty(name)) {
+                    updateSchool(schoolID, name, province, city, email, address, phone, postalCode);
+                    alertDialog.dismiss();
+                } else {
+                    updateSchoolName.setError("Name is required");
+                    return;
+                }
+            }
+        });
+
+        deleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                deleteSchool(schoolID);
             }
         });
     }
+
+    private void deleteSchool(String id) {
+        DatabaseReference deleteSchools = FirebaseDatabase.getInstance().getReference("schools").child(id);
+        DatabaseReference deleteCourses = FirebaseDatabase.getInstance().getReference("courses").child(id);
+
+        deleteSchools.removeValue();
+        deleteCourses.removeValue();
+
+        Toast.makeText(this, "School is deleted", Toast.LENGTH_LONG).show();
+    }
+
+    public boolean updateSchool(String id, String name, String province, String city, String email, String address, String phone, String postalCode) {
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("schools").child(id);
+        School school = new School(id, name, province, city, email, address, phone, postalCode);
+        databaseReference.setValue(school);
+        Toast.makeText(this, "School Updated", Toast.LENGTH_LONG).show();
+        return true;
+    }
+
 }
